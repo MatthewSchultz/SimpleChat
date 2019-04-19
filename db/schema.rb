7797b0dd2894a86e8bef60a07b99b7fff4c69c78
Matthew Schultz <abstractapproach@gmail.com>
2019-04-19 14:20:39 -0500

    Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_183908) do
+ActiveRecord::Schema.define(version: 2019_04_19_191251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "cube"
@@ -19,5 +19,14 @@ ActiveRecord::Schema.define(version: 2019_04_19_183908) do
   enable_extension "intarray"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
 
 end
